681858c8f96e238a05b459a0706945d5d52b2ee6
siwka <ms.ksiwek@gmail.com>
2015-01-06 16:45:34 -0600

    Make a basic User model with password

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,5 +6,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
+    get signup_path
+    assert_select 'title', "Sign up"
 	end
 end
