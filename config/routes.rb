a7c75358f346814835a1f5d0cb1cdc6dd5459802
siwka <ms.ksiwek@gmail.com>
2015-01-06 21:31:12 -0600

    Session based only log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  root to: 'static_pages#home'
-  match '/signup',  to: 'users#new', via: 'get'
+  root  to: 'static_pages#home'
+  match '/signup',  to: 'users#new',        via: 'get'
+  match '/login',		to: 'sessions#new',     via: 'get'
+  match '/login',   to: 'sessions#create',  via: 'post'
+  match '/logout',  to: 'sessions#destroy', via: 'delete'
   resources :users
 end
